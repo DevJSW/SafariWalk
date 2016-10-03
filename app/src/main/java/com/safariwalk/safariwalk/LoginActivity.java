56a4690539d29fb2b49d59d7001f237717d5d41d
DevJSW <johnwasike0@gmail.com>
2016-10-03 09:40:26 -0900

    adding login file

---
+++ app/src/main/java/com/safariwalk/safariwalk/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.safariwalk.safariwalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    private Button mLoginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmailField = (EditText) findViewById(R.id.emailField);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+        
+        mLoginBtn = (Button) findViewById(R.id.loginBtn);
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUserExists();
+            }
+        });
+    }
+
+    private void checkUserExists() {
+    }
+}
--- app/src/main/java/com/safariwalk/safariwalk/SignupActivity.java
+++ app/src/main/java/com/safariwalk/safariwalk/SignupActivity.java
@@ -1,21 +1,31 @@
 package com.safariwalk.safariwalk;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
 
     private EditText mEmailField;
+    private EditText mNameField;
     private EditText mPasswordField;
 
     private Button mSignupBtn;
 
+    private ProgressDialog mProgress;
+    private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
 
     @Override
@@ -25,7 +35,11 @@ public class SignupActivity extends AppCompatActivity {
 
         mEmailField = (EditText) findViewById(R.id.emailField);
         mPasswordField = (EditText)findViewById(R.id.passwordField);
+        mNameField = (EditText) findViewById(R.id.nameField);
 
+        mProgress = new ProgressDialog(this);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("User");
         mSignupBtn = (Button) findViewById(R.id.signupBtn);
         mSignupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,12 +53,27 @@ public class SignupActivity extends AppCompatActivity {
 
     private void signupUser() {
 
-        String email = mEmailField.getText().toString().trim();
-        String password = mPasswordField.getText().toString().trim();
+        final String name = mNameField.getText().toString().trim();
+        final String email = mEmailField.getText().toString().trim();
+        final String password = mPasswordField.getText().toString().trim();
 
-       if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+       if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
 
-          // mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener()
+          mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+              @Override
+              public void onComplete(@NonNull Task<AuthResult> task) {
+
+                  if (task.isSuccessful()) {
+
+                      String User_id = mAuth.getCurrentUser().getUid();
+                      DatabaseReference current_userID = mDatabase.child("User_id");
+                      current_userID.child("name").setValue(name);
+                      current_userID.child("image").setValue("default");
+
+                  }
+
+              }
+          });
 
        }else {
 
