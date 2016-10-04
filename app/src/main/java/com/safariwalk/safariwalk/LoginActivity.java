ee6889c0479f27c86dac966c81b9285d4a16a606
DevJSW <johnwasike0@gmail.com>
2016-10-04 10:50:26 -0900

    commiting changes on loginActivity

--- app/src/main/java/com/safariwalk/safariwalk/LoginActivity.java
+++ app/src/main/java/com/safariwalk/safariwalk/LoginActivity.java
@@ -1,15 +1,32 @@
 package com.safariwalk.safariwalk;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText mEmailField;
     private EditText mPasswordField;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
     private Button mLoginBtn;
 
@@ -20,19 +37,60 @@ public class LoginActivity extends AppCompatActivity {
 
         mEmailField = (EditText) findViewById(R.id.emailField);
         mPasswordField = (EditText) findViewById(R.id.passwordField);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
         
         mLoginBtn = (Button) findViewById(R.id.loginBtn);
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkUserExists();
+                UserLogin();
             }
         });
     }
 
-    private void checkUserExists() {
+    private void UserLogin() {
 
         String email = mEmailField.getText().toString().trim();
+        String password = mPasswordField.getText().toString().trim();
 
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                   if (task.isSuccessful()) {
+                       checkUserExist();
+                   }
+                }
+            });
+        }
+
+
+    }
+
+    private void checkUserExist() {
+
+        final String User_id = mAuth.getCurrentUser().getUid();
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChild(User_id)) {
+
+                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(mainIntent);
+
+                }else {
+                    Toast.makeText(LoginActivity.this, "You need to set up your account", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
